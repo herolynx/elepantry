29f069dc19562399228947e8bf5f7f3b3b5bc707
Michal Wronski <michal.robert.wronski@gmail.com>
2016-03-03 15:00:04 +0100

    Resource file repository added.

---
+++ src/main/kotlin/com/herolynx/barefoot/repository/ResourceFileRepository.kt
@@ -0,0 +1,17 @@
+package com.herolynx.barefoot.repository
+
+import org.funktionale.either.Either
+import java.io.File
+
+/**
+ * Repository that gives access to physical files
+ *
+ * @author Michal Wronski
+ */
+interface ResourceFileRepository {
+
+    fun download(id: String): Either<Exception, File?>
+
+    fun upload(f: File): Either<Exception, Boolean>
+
+}
