a6056927794f83aa5100c1a0dbed035f8af0ec25
Michal Wronski <michal.robert.wronski@gmail.com>
2016-03-11 22:22:26 +0100

    [#2] Google drive API support added.
    Resources loaded from goodle drive in a lazy way.
    Wireframes moved to main project dir.
    OnClick changed to onTouchTap.
    Showing/hidding of the right menu added.
    Free text searching operation added.
    Downloading of resources added.
    Previewing of resources added.

--- web/app/scripts/domain/storage/google-drive-store.jsx
+++ web/app/scripts/domain/storage/google-drive-store.jsx
@@ -2,36 +2,65 @@ import Reflux from 'reflux';
 
 import Google from '../drivers/google-driver';
 
+/**
+ * Convert google file item into resource model
+ * @return resource object
+*/
 function toResource(item) {
-  return {id: item.id, file: item, name: item.title, url: item.downloadUrl, type: item.fileExtension};
-}
-
-function retrieveAllFiles(resolve, reject) {
-  let retrievePageOfFiles = (request, result) => {
-    request.execute(resp => {
-      result = result.concat(resp.items.map(item => toResource(item)));
-      var nextPageToken = resp.nextPageToken;
-      if (nextPageToken) {
-        request = gapi.client.drive.files.list({'pageToken': nextPageToken});
-        retrievePageOfFiles(request, result);
-      } else {
-        resolve(result);
-      }
-    });
-  }
-  let initialRequest = gapi.client.drive.files.list();
-  retrievePageOfFiles(initialRequest, []);
+  return {
+    id: item.id,
+    name: item.title,
+    previewUrl: item.webContentLink != undefined ? item.webContentLink.replace(/&export=download/, '') : undefined,
+    downloadUrl: item.webContentLink,
+    fileExtension: item.fileExtension,
+    storage: 'google-drive'
+  };
 }
 
 let googleDrive = Reflux.createStore({
 
-  list: function(pageToken) {
-    console.debug('Listing all files from google drive...');
+  /**
+  * List info about google files
+  * @param pageToken optional page to be displayed (paging support)
+  * @param fileNameQuery optional free text for file name searching
+  * @param maxResults optional max results to be loaded
+  * @return promise tuple of results and next page token
+  */
+  list: function(pageToken, fileNameQuery='', maxResults = 100) {
+    console.debug('Listing all files from google drive - maxResults: ', maxResults);
     return Google.loadClients().then((gapi) => {
       return new Promise((resolve, reject) => {
-        retrieveAllFiles(resolve, reject);
+        let query = `title contains '${fileNameQuery}'`;
+        let request = gapi.client.drive.files.list({'pageToken': pageToken, 'maxResults': maxResults, q: query});
+        request.execute(resp => {
+          let result = resp.items.map(item => toResource(item));
+          resolve([result, resp.nextPageToken]);
+        });
       });
+    });
+  },
 
+  /**
+  * Download file
+  * @param resource info about file
+  * @return promise promise of downloaded file content
+  */
+  download: function(resource) {
+    console.debug('Downloading from google drive: ', resource);
+    return Google.loadClients().then((gapi) => {
+      return new Promise((resolve, reject) => {
+        let accessToken = gapi.auth.getToken().access_token;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', resource.downloadUrl);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+        xhr.onload = function() {
+          resolve(xhr.responseText);
+        };
+        xhr.onerror = function(e) {
+          reject(e);
+        };
+        xhr.send();
+      });
     });
   }
 
--- web/app/scripts/web/views/resource-view.jsx
+++ web/app/scripts/web/views/resource-view.jsx
@@ -1,31 +1,52 @@
 import React from 'react';
-
+import InfiniteScroll from 'react-infinite-scroller';
 import GoogleDrive from '../../domain/storage/google-drive-store';
-import File from './file';
+import Resource from './resource';
+
+/**
+* Create view for given resource
+* @param resource resource to be displayed
+* @return JSX resource view
+*/
+function createResourceView(resource) {
+  return (
+    <Resource key={resource.id} resource={resource} />
+  );
+}
 
 let resourceView = React.createClass({
 
   getInitialState: function() {
     return {
-      googleDrive: []
+      resources: [],
+      nextPageToken: undefined,
+      hasMore: true
     };
   },
 
-  componentDidMount: function() {
-    console.debug('Resource view - loading list of files...');
-    GoogleDrive.list().then(files => {
-      console.debug('Google drive files received - size: ', files.length);
-      this.setState({googleDrive: files});
-    });
+  /**
+  * Load resources
+  */
+  load: function() {
+    console.debug('Resource view - loading - next page token: ', this.state.nextPageToken);
+    GoogleDrive
+      .list(this.state.nextPageToken)
+      .then(([files, nextPageToken]) => {
+        console.debug('Google drive resources loaded:', files.length, nextPageToken);
+        let newState = this.state;
+        newState.resources.push(files.map(file => createResourceView(file)));
+        newState.nextPageToken = nextPageToken;
+        newState.hasMore = nextPageToken !== undefined;
+        this.setState(newState);
+      });
   },
 
   render: function() {
     return (
       <div id="files" className="list">
-        {this.state.googleDrive.map(googleFile => {
-          return <File googleFile={googleFile}/>;
-        })}
-
+        <InfiniteScroll loader={<div className="loader">Loading ...</div>} loadMore={this.load} hasMore={this.state.hasMore} useWindow={false}>
+          {this.state.resources}
+        </InfiniteScroll>
       </div>
     );
   }
